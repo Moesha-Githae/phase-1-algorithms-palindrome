34b041aa31ec2d539d8410474bdb3082fcd6be66
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-04-02 15:20:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split("").reverse().join("")
+  return reversed === word
 }
 
 /* 
   Add your pseudocode here
+  1. split string to individual characters forming an array
+  2. call reverse method on the array
+  3. join the array
+  4. check if joined array is the same as input word
+  5. if same return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  the method uses split() to conver input word into an array of characters.
+  using the array reverse method, the array is reversed so the last letter comes first
+  joining the array creates a new word that can be compared with the input value 
+  and when they are same return a value of true else return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +31,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting false");
+  console.log("=>", isPalindrome("mother"));
+
+  console.log("");
+
+  console.log("Expecting true");
+  console.log("=>", isPalindrome("refer"));
 }
 
 module.exports = isPalindrome;
